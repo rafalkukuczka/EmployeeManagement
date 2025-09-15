af8051829023e648ef29a61b734c6cb89038e39f
rkukuczka <rafal.qqczka@gmail.com>
2025-09-15 20:50:25 +0200

    Defined Application Interfaces

--- EmployeeManagement.Core/Domain/Employee/Interfaces/IEmployeeNumberGenerator.cs
+++ EmployeeManagement.Core/Domain/Employee/Interfaces/IEmployeeNumberGenerator.cs
@@ -6,8 +6,9 @@ using System.Threading.Tasks;
 
 namespace EmployeeManagement.Core.Domain.Employee.Interfaces
 {
+
     public interface IEmployeeNumberGenerator
     {
-
+        EmployeeNumber NextNumber(IEnumerable<Employee> employees);
     }
 }
---
+++ EmployeeManagement.Core/Domain/Employee/Interfaces/IEmployeeRepository.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EmployeeManagement.Core.Domain.Employee.Interfaces
+{
+    public interface IEmployeeRepository
+    {
+        
+        Task AddAsync(Employee employee);
+        Task<IEnumerable<Employee>> GetAllAsync();
+        Task<Employee?> GetByIdAsync(Guid id);
+        Task UpdateAsync(Employee employee);
+
+    }
+}
